1cb08468398ad585583080a9e166f628309c1423
Rafael Ponte <ilderdamasceno@gmail.com>
2022-01-16 01:46:13 -0300

    GG

---
+++ Scripts/GigantEnemy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GigantEnemy : EnemyBase 
+{
+
+	void Start () {}
+	
+	// Update is called once per frame
+	protected override void Update() 
+	{
+		base.Update();	
+	}
+
+	private void FixedUpdate()
+	{
+		if(Mathf.Abs(playerDistance) < atackDistance)
+		{
+			StartCoroutine(movi());
+		} else {
+			StopCoroutine(movi());
+		}
+	}
+
+	IEnumerator movi()
+	{	if(facingRight)
+		{
+			enemyBody.velocity = new Vector2(-enemySpeed / 2, enemySpeed);
+		} else {
+			enemyBody.velocity = new Vector2(enemySpeed / 2, enemySpeed);
+		}
+		yield return new WaitForSeconds(1f);
+		if(facingRight)
+		{
+			enemyBody.velocity = new Vector2(-enemySpeed / 2, enemySpeed);
+		} else {
+			enemyBody.velocity = new Vector2(enemySpeed / 2, enemySpeed);
+		}
+		yield return new WaitForSeconds(1f);
+		if(facingRight)
+		{
+			enemyBody.velocity = new Vector2(-enemySpeed / 2, enemySpeed);
+		} else {
+			enemyBody.velocity = new Vector2(enemySpeed / 2, enemySpeed * 1.5f);
+		}
+		yield return new WaitForSeconds(1.5f);
+	}
+	
+}
